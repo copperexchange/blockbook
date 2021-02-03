c2fd102272fbc38dfda5acd6261fccce846c0db3
Andrey Pshenkin <andrey.pshenkin@gmail.com>
2021-02-04 00:06:31 +0300

    fix typo

--- api/worker.go
+++ api/worker.go
@@ -1269,7 +1269,7 @@ func (w *Worker) getAddrDescUtxo(addrDesc bchain.AddressDescriptor, ba *db.AddrB
 							Height:        int(utxo.Height),
 							Confirmations: confirmations,
 							Coinbase:      coinbase,
-							Hex:           vout.ScriptPubKey.Hex,
+							Hex:           utxo.ScriptPubKey.Hex,
 						})
 					}
 				}
